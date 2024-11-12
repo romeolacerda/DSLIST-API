d061dd12b86cbb48e75fe7884e6b79c7af1e4660
romeo <romeolacerdafarias@gmail.com>
2024-11-12 09:24:41 -0300

    project init

---
+++ src/main/java/com/rle/dslistapi/DslistApiApplication.java
@@ -0,0 +1,13 @@
+package com.rle.dslistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslistApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rle/dslistapi/DslistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rle.dslistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
